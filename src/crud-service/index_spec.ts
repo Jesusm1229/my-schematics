06a577301d6427b07a0c5eb4c981202fe2035237
jesusm1229 <jesusmedina1229@gmail.com>
2022-01-07 09:54:21 -0400

    Adding crud-services squematics

--- src/crud-service/index_spec.ts
+++ src/crud-service/index_spec.ts
@@ -6,10 +6,10 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 
-describe('tailwind-container', () => {
+describe('crud-service', () => {
   it('works', async () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('tailwind-container', {}, Tree.empty()).toPromise();
+    const tree = await runner.runSchematicAsync('crud-service', {}, Tree.empty()).toPromise();
 
     expect(tree.files).toEqual([]);
   });
